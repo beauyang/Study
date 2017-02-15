83a70fb3eb8334dddb65828b68be8eeef3e4253b
yangbo <yangbo3306@gmail.com>
2017-02-15 23:13:31 +0800

    learn something new

--- h5/17/17.2/js/work.js
+++ h5/17/17.2/js/work.js
@@ -5,19 +5,19 @@ onmessage = function (event) {
     //将数据提取出来
     var data = JSON.parse(event.data);
     //取出start参数
-    var start = data.start;
+    var start1 = data.start1;
     //取出end参数
-    var end = data.end;
+    var end1 = data.end1;
     var result = "";
     search :
-    for (var n = start; n <= end; n++){
+    for (var n = start1; n <= end1; n++){
         for(var i = 2;i <= Math.sqrt(n); i++){
             if (n % 1 == 0) {
                 continue search;
             }
         }
+        result += (n + ",");
     }
-    result += (n + ',');
     //发送消息，会触发前台JavaScript脚本中Worker对象的onmessage
     postMessage(result);
 };
